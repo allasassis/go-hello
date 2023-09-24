5117a0292f88dff2de6f72001f87affb572cd0cc
Allas Assis <allasassis10@gmail.com>
2023-09-24 20:26:57 -0300

    Exploring functions and monitoring websites

--- hello.go
+++ hello.go
@@ -2,23 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
-	command := showIntroduction()
-	showMenu(command)
+	for {
+		command := showIntroduction()
+		showMenu(command)
+	}
 }
 
 func showIntroduction() int {
 	/* When working with Go, every function coming from an external package starts with an uppercase letter. */
 	/* name := "Allas" This is a way of declaring a variable without specifying its type. */
-
-	fmt.Println("Hey, what's your name?")
-	var name string
-	fmt.Scan(&name)
-
-	fmt.Println("Hello,", name, "! How's it going?")
+	fmt.Println("Hello! How's it going?")
 	fmt.Println("1 - Start monitoring")
 	fmt.Println("2 - Show logs")
 	fmt.Println("0 - Exit")
@@ -32,7 +30,7 @@ func showIntroduction() int {
 func showMenu(command int) {
 	switch command {
 	case 1:
-		fmt.Println("Starting monitoring!")
+		startMonitoring()
 	case 2:
 		fmt.Println("Showing logs...")
 	case 0:
@@ -41,5 +39,19 @@ func showMenu(command int) {
 	default:
 		fmt.Println("I do not recognize your command.")
 		os.Exit(-1)
+	}
+}
+
+func startMonitoring() {
+	fmt.Println("Starting monitoring..")
+	site := "https://www.google.com"
+
+	// resp, error := http.Get(site)
+	resp, _ := http.Get(site)
+	// The _ indicates that you want to ignore that respective variable
+	if resp.StatusCode == 200 {
+		fmt.Println("Everything is fine with the website:", site)
+	} else {
+		fmt.Println("Unfortunately, the website ", site, "isn't working well, the status code is: ", resp.StatusCode)
 	}
 }
